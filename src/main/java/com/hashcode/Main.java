c2890042dfea79a909dcbef907a93fd4ae90c0db
Your Name <you@example.com>
2018-03-01 18:42:29 +0100

    Initial commit

---
+++ src/main/java/com/hashcode/Main.java
@@ -0,0 +1,9 @@
+package com.hashcode;
+
+public class Main {
+
+    public static void main(final String args[]) {
+        System.out.println("hola");
+    }
+
+}
---
+++ src/test/java/com/hashcode/MainTest.java
@@ -0,0 +1,14 @@
+package com.hashcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        Assert.assertEquals(true, true);
+    }
+}
